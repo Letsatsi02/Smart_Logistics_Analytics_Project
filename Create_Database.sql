47811bf27b485b7b84fdaf94b8d8463030927561
Letsatsi Matlala <142995828+Letsatsi02@users.noreply.github.com>
2026-03-24 16:19:08 +0200

    creating_customer_table

--- Create_Database.sql
+++ Create_Database.sql
@@ -1,3 +1,20 @@
 -- Create Database
 CREATE DATABASE smart_logistics;
 GO
+USE smart_logistics;
+GO
+-- DIMENSION TABLES
+-- ============================================================
+
+-- Dim_Customer
+CREATE TABLE Dim_Customer (
+    CustomerID      INT             PRIMARY KEY,
+    FullName        NVARCHAR(100)   NOT NULL,
+    PhoneNumber     NVARCHAR(20),
+    Email           NVARCHAR(100),
+    CustomerType    NVARCHAR(50),
+    LocationID      INT,
+    JoinDate        DATE,
+    IsActive        BIT             DEFAULT 1       -- 1 = TRUE, 0 = FALSE
+);
+GO
