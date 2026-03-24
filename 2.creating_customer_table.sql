be5290cb56e097d9d710ae9e97a40f132db5a8f3
Letsatsi Matlala <142995828+Letsatsi02@users.noreply.github.com>
2026-03-24 05:09:38 +0200

    creating_customer_table

---
+++ 2.creating_customer_table.sql
@@ -0,0 +1,12 @@
+-- Dim_Customer
+CREATE TABLE Dim_Customer (
+    CustomerID      INT             PRIMARY KEY,
+    FullName        NVARCHAR(100)   NOT NULL,
+    PhoneNumber     NVARCHAR(20),
+    Email           NVARCHAR(100),
+    CustomerType    NVARCHAR(50),
+    LocationID      INT,
+    JoinDate        DATE,
+    IsActive        BIT             DEFAULT 1       -- 1 = TRUE, 0 = FALSE
+);
+GO
